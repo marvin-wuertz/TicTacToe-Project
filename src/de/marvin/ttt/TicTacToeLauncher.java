60bbfa23c6f3816914c4a838a30c959306aa952d
Marvin Würtz <marvin.wuertz03@gmail.com>
2020-06-07 01:18:26 +0200

    Initial Commit

--- src/de/marvin/ttt/TicTacToeLauncher.java
+++ src/de/marvin/ttt/TicTacToeLauncher.java
@@ -3,11 +3,17 @@ package de.marvin.ttt;
 //import de.marvin.ttt.engine.Game;
 import de.marvin.ttt.gui.Frames;
 
+/**
+ * @author Marvin Würtz
+ * 
+ * Copyright 06.06.2020 Marvin Würtz
+ * TicTacToe-Project
+ */
 public class TicTacToeLauncher {
 	
 	public static void main(String[] args) {
 		Frames frames = new Frames();
-		frames.startSetupFrame();
+		frames.startGameFrame();
 		
 		//Game game = new Game();
 	}
--- src/de/marvin/ttt/engine/Game.java
+++ src/de/marvin/ttt/engine/Game.java
@@ -1,5 +1,11 @@
 package de.marvin.ttt.engine;
 
+/**
+ * @author Marvin Würtz
+ * 
+ * Copyright 06.06.2020 Marvin Würtz
+ * TicTacToe-Project
+ */
 public class Game {
 
 }
